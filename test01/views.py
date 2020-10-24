9c23f877271ab6f65d036fedff1faf9625115a01
jiemoo <978090944@qq.com>
2020-10-24 15:01:44 +0800

    staticfiles

--- test01/views.py
+++ test01/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from templates import *
+
 
 # Create your views here.
+def timer(requset):
+    import time
+    context = {'time': time.time()}
+    return render(requset, template_name='timer.html', context=context)
