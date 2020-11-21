c8420adc2cc6f2e3c4556b4c5ca6e57daeb9fd3a
jiemoo <978090944@qq.com>
2020-11-21 17:01:45 +0800

    staticfiles

--- sm/settings.py
+++ sm/settings.py
@@ -28,6 +28,8 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 
+LOGIN_URL = '/login/'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -54,7 +56,10 @@ ROOT_URLCONF = 'sm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "test01", 'template'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
